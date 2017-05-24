81df6d455f5ff45cd0cc1b1ff5f4f002234cef64
Matthew Baggett <matthew@baggett.me>
2017-05-24 18:55:07 +0100

    Move getAll into Service Abstract instead of being generated over and over in generated services

--- src/Abstracts/Service.php
+++ src/Abstracts/Service.php
@@ -1,6 +1,9 @@
 <?php
 namespace Segura\AppCore\Abstracts;
 
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\Sql\Select;
+
 abstract class Service
 {
     abstract public function getNewModelInstance();
@@ -8,4 +11,54 @@ abstract class Service
     abstract public function getTermPlural() : string;
 
     abstract public function getTermSingular() : string;
+
+    abstract public function getNewTableGatewayInstance();
+
+    /**
+     * @param int|null              $limit
+     * @param int|null              $offset
+     * @param array|\Closure[]|null $wheres
+     * @param string|null           $order
+     * @param string|null           $orderDirection
+     *
+     * @return Model[]
+     */
+    public function getAll(
+        int $limit = null,
+        int $offset = null,
+        array $wheres = null,
+        string $order = null,
+        string $orderDirection = null
+    ) {
+        /** @var TableGateway $tableGateway */
+        $tableGateway              = $this->getNewTableGatewayInstance();
+        list($matches, $count)     = $tableGateway->fetchAll(
+            $limit,
+            $offset,
+            $wheres,
+            $order,
+            $orderDirection !== null ? $orderDirection : Select::ORDER_ASCENDING
+        );
+        $return = [];
+
+        if ($matches instanceof ResultSet) {
+            foreach ($matches as $match) {
+                $return[] = $match;
+            }
+        }
+        return $return;
+    }
+
+    /**
+     * @param array|\Closure[]|null $wheres
+     *
+     * @return int
+     */
+    public function countAll(
+        array $wheres = null
+    ) {
+        /** @var TableGateway $tableGateway */
+        $tableGateway              = $this->getNewTableGatewayInstance();
+        return $tableGateway->getCount($wheres);
+    }
 }
