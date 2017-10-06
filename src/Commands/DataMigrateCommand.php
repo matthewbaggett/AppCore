1784189b342dc65da467bafdf7a91c413f15ce6d
Matthew Baggett <matthew@baggett.me>
2017-10-06 14:11:46 +0100

    Interrogate zenderator.yml to see if there are some SQL files to run/migrate.

--- src/Commands/DataMigrateCommand.php
+++ src/Commands/DataMigrateCommand.php
@@ -4,13 +4,22 @@ namespace Segura\AppCore\Commands;
 use Segura\AppCore\App;
 use Segura\AppCore\Services\AutoImporterService;
 use Zenderator\Automize;
+use Zenderator\Zenderator;
 
 class DataMigrateCommand extends Automize\AutomizeCommand implements Automize\AutomizeCommandInterface
 {
     public function action() : bool
     {
+        $rootOfApp = '/app';
         /** @var AutoImporterService $autoImporter */
         $autoImporter = App::Container()->get(AutoImporterService::class);
+        $config = Zenderator::getConfig($rootOfApp);
+        $autoImporter->addSqlPath($rootOfApp . "/vendor/segura/appcore/src/SQL");
+        if(isset($config['sql'])){
+            foreach($config['sql'] as $location){
+                $autoImporter->addSqlPath($rootOfApp . "/" . $location);
+            }
+        }
         $autoImporter->run();
         echo "\n\n";
         $this->getZenderator()->waitForKeypress();
