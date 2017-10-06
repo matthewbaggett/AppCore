adb08929d5826d99b3270e9184b9bdfdaacecffc
Matthew Baggett <matthew@baggett.me>
2017-10-06 14:14:18 +0100

    Also apply it to the Revet command.

--- src/Commands/DataRevertCommand.php
+++ src/Commands/DataRevertCommand.php
@@ -4,13 +4,22 @@ namespace Segura\AppCore\Commands;
 use Segura\AppCore\App;
 use Segura\AppCore\Services\AutoImporterService;
 use Zenderator\Automize;
+use Zenderator\Zenderator;
 
 class DataRevertCommand extends Automize\AutomizeCommand implements Automize\AutomizeCommandInterface
 {
     public function action() : bool
     {
+        $rootOfApp = '/app';
         /** @var AutoImporterService $autoImporter */
         $autoImporter = App::Container()->get(AutoImporterService::class);
+        $config = Zenderator::getConfig($rootOfApp);
+        $autoImporter->addSqlPath($rootOfApp . "/vendor/segura/appcore/src/SQL");
+        if(isset($config['sql'])){
+            foreach($config['sql'] as $location){
+                $autoImporter->addSqlPath($rootOfApp . "/" . $location);
+            }
+        }
         $autoImporter->purge();
         echo "\n\n";
         $autoImporter->run();
