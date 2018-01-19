018d3ac190ac8f9e4e70fa70c213189a8f4182e6
Matthew Baggett <matthew@baggett.me>
2018-01-19 11:52:51 +0000

    remove unused uses.

--- src/Controllers/RedirectorController.php
+++ src/Controllers/RedirectorController.php
@@ -2,12 +2,8 @@
 namespace Segura\AppCore\Controllers;
 
 use Segura\AppCore\Abstracts\Controller;
-use Segura\AppCore\App;
-use Segura\AppCore\Router\Router;
 use Slim\Http\Request;
 use Slim\Http\Response;
-use Slim\Route;
-use Slim\Views\Twig;
 
 class RedirectorController extends Controller
 {
