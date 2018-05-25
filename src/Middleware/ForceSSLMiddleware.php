b14227e6e86a72758ac780bd40fe3f72bec7193c
Matthew Baggett <matthew@baggett.me>
2018-05-25 23:11:51 +0100

    Wrong way around for the comparison.

--- src/Middleware/ForceSSLMiddleware.php
+++ src/Middleware/ForceSSLMiddleware.php
@@ -10,7 +10,7 @@ class ForceSSLMiddleware
     public function __invoke(Request $request, Response $response, $next)
     {
         /** @var Response $response */
-        if ($request->getServerParam('SERVER_PORT') == '80' && !strtolower($request->getServerParam('FORCE_HTTPS')) == 'yes') {
+        if ($request->getServerParam('SERVER_PORT') == '80' && strtolower($request->getServerParam('FORCE_HTTPS')) == 'yes') {
             return $response->withRedirect("https://" . $request->getServerParam('HTTP_HOST') . "/" . ltrim($request->getServerParam('REQUEST_URI'),"/"));
         }
         $response = $next($request, $response);
