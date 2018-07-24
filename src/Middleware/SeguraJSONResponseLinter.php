e7c5cc22d625ced3cc2f47e78d8a9c2023c07464
Matthew Baggett <matthew@baggett.me>
2018-07-24 21:04:43 +0100

    Add a Segura JSON Response Linter to lint to a standard response expectation.

---
+++ src/Middleware/SeguraJSONResponseLinter.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Segura\AppCore\Middleware;
+
+use Segura\AppCore\App;
+use Segura\AppCore\Controllers\InlineCssTrait;
+use Segura\AppCore\Zend\Profiler;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class SeguraJSONResponseLinter
+{
+
+    protected $apiExplorerEnabled = true;
+
+    public function __invoke(Request $request, Response $response, $next)
+    {
+        /** @var Response $response */
+        $response = $next($request, $response);
+        if (isset($response->getHeader('Content-Type')[0])
+            and stripos($response->getHeader('Content-Type')[0], 'application/json') !== false
+        ) {
+            $body = $response->getBody();
+            $body->rewind();
+
+            $json = json_decode($body->getContents(), true);
+
+            if (isset($json['Status'])) {
+                $json['Status'] = ucfirst(strtolower($json['Status']));
+            }
+
+            $response = $response->withJson($json, null, JSON_PRETTY_PRINT);
+        }
+
+        return $response;
+    }
+}
