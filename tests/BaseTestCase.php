1c892604f082da657087b9a11d60e3d28c2797d6
Matthew Baggett <matthew@baggett.me>
2018-10-04 17:41:29 +0100

    Enable running tests without a database.

--- tests/BaseTestCase.php
+++ tests/BaseTestCase.php
@@ -51,19 +51,26 @@ abstract class BaseTestCase extends TestCase
         $this->app = $app;
     }
 
+    private static function isTestDatabaseEnabled() : bool
+    {
+        /** @var EnvironmentService $environment */
+        $environment = self::getAppContainer()->get(EnvironmentService::class);
+        return $environment->isSet('MYSQL_HOST') || $environment->isSet('MYSQL_PORT');
+    }
+
     public static function setUpBeforeClass()
     {
         self::$startTime = microtime(true);
 
-        /** @var EnvironmentService $environment */
-        $environment = self::getAppContainer()->get(EnvironmentService::class);
+        // If MySQL has been configured, enable a transaction that we can rollback later.
+        if(self::isTestDatabaseEnabled()) {
+            App::Instance()->getContainer()->get(Db::class);
 
-        App::Instance()->getContainer()->get(Db::class);
-
-        // If MySQL has been configured, begin transaction.
-        if (Db::isMySQLConfigured()) {
-            foreach (Db::getInstance()->getDatabases() as $name => $database) {
-                $database->driver->getConnection()->beginTransaction();
+            // If MySQL has been configured, begin transaction.
+            if (Db::isMySQLConfigured()) {
+                foreach (Db::getInstance()->getDatabases() as $name => $database) {
+                    $database->driver->getConnection()->beginTransaction();
+                }
             }
         }
 
@@ -74,12 +81,11 @@ abstract class BaseTestCase extends TestCase
     public static function tearDownAfterClass()
     {
         // If MySQL has been configured, roll back transaction.
-        /** @var EnvironmentService $environment */
-        $environment = self::getAppContainer()->get(EnvironmentService::class);
-
-        if (Db::isMySQLConfigured()) {
-            foreach (Db::getInstance()->getDatabases() as $name => $database) {
-                $database->driver->getConnection()->rollback();
+        if(self::isTestDatabaseEnabled()) {
+            if (Db::isMySQLConfigured()) {
+                foreach (Db::getInstance()->getDatabases() as $name => $database) {
+                    $database->driver->getConnection()->rollback();
+                }
             }
         }
 
