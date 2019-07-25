50f73be14a24055f1972efb4e83cb09bf66b76e0
Matthew Baggett <matthew@baggett.me>
2019-07-25 13:55:20 +0200

    reformatting.

--- src/Redis/Redis.php
+++ src/Redis/Redis.php
@@ -25,6 +25,8 @@ class Redis implements ClientInterface
     protected const CLIENTS_ALL = 0;
     protected const CLIENTS_READONLY = 1;
     protected const CLIENTS_WRITEONLY = 2;
+    protected const SINGLE_ARG_COMMANDS = ["get", "set"];
+
     protected static $clusterConfiguration;
     protected static $clusterConfigurationLastUpdated;
     /** @var EnvironmentService */
@@ -82,12 +84,6 @@ class Redis implements ClientInterface
         #!\Kint::dump(self::$clusterConfiguration, "Configuration is " . (self::$clusterConfigurationLastUpdated - time()) . " seconds old");
     }
 
-    protected function clearClusterConfig() : void
-    {
-        self::$clusterConfiguration = null;
-        self::$clusterConfigurationLastUpdated = null;
-    }
-
     protected function configureCluster(): void
     {
         if (!(!self::$clusterConfiguration || self::$clusterConfigurationLastUpdated <= time() - self::CLUSTER_CONFIGURATION_MAX_AGE_SECONDS)) {
@@ -120,7 +116,7 @@ class Redis implements ClientInterface
             $type = in_array("master", $flags) ? 'master' : 'slave';
 
             // Split our supportedHashRsange if we've got one.
-            if(isset($supportedHashRange) && stripos($supportedHashRange, "-") !== false) {
+            if (isset($supportedHashRange) && stripos($supportedHashRange, "-") !== false) {
                 list($hashMin, $hashMax) = explode("-", $supportedHashRange, 2);
             }
 
@@ -141,38 +137,6 @@ class Redis implements ClientInterface
         });
 
         self::$clusterConfigurationLastUpdated = time();
-    }
-
-    public function getServerByKey(string $key) : Client
-    {
-        return $this->getServerByHash(
-            $this->clusterStrategy->getSlotByKey($key)
-        );
-    }
-    /**
-     * @param int $hash
-     * @return Client
-     * @throws Exception
-     */
-    public function getServerByHash(int $hash) : Client
-    {
-        $this->clearClusterConfig();
-
-        // Check for Update to Cluster information, if its time
-        $this->configureCluster();
-
-        // Loop over our cluster information, and determine if we're in between the hashmins and maxes.
-        foreach(self::$clusterConfiguration as $clusterNodeConfiguration){
-            if(isset($clusterNodeConfiguration['hashMin']) && isset($clusterNodeConfiguration['hashMax'])){
-                if($hash >= $clusterNodeConfiguration['hashMin'] && $hash <= $clusterNodeConfiguration['hashMax']){
-                    //\Kint::dump($clusterNodeConfiguration['connection']);
-                    return $this->getClientByAddress($clusterNodeConfiguration['connection']);
-                }
-            }
-        }
-
-        // If we've gotten this far, something is chronically borked.
-        throw new Exception("Cannot find a Redis Server that is accepting hash {$hash}...");
     }
 
     protected function getClient($mode = self::CLIENTS_ALL): Client
@@ -197,6 +161,55 @@ class Redis implements ClientInterface
         }
 
         return $clients;
+    }
+
+    public function getServerByKey(string $key): Client
+    {
+        return $this->getServerByHash(
+            $this->clusterStrategy->getSlotByKey($key)
+        );
+    }
+
+    /**
+     * @param int $hash
+     * @return Client
+     * @throws Exception
+     */
+    public function getServerByHash(int $hash): Client
+    {
+        $this->clearClusterConfig();
+
+        // Check for Update to Cluster information, if its time
+        $this->configureCluster();
+
+        // Loop over our cluster information, and determine if we're in between the hashmins and maxes.
+        foreach (self::$clusterConfiguration as $clusterNodeConfiguration) {
+            if (isset($clusterNodeConfiguration['hashMin']) && isset($clusterNodeConfiguration['hashMax'])) {
+                if ($hash >= $clusterNodeConfiguration['hashMin'] && $hash <= $clusterNodeConfiguration['hashMax']) {
+                    //\Kint::dump($clusterNodeConfiguration['connection']);
+                    return $this->getClientByAddress($clusterNodeConfiguration['connection']);
+                }
+            }
+        }
+
+        // If we've gotten this far, something is chronically borked.
+        throw new Exception("Cannot find a Redis Server that is accepting hash {$hash}...");
+    }
+
+    protected function clearClusterConfig(): void
+    {
+        self::$clusterConfiguration = null;
+        self::$clusterConfigurationLastUpdated = null;
+    }
+
+    protected function getClientByAddress($address): ?Client
+    {
+        foreach ($this->getClients(self::CLIENTS_ALL) as $client) {
+            if (in_array($address, $client->getConnectionDetails())) {
+                return $client;
+            }
+        }
+        return null;
     }
 
     public function getProfile()
@@ -239,7 +252,7 @@ class Redis implements ClientInterface
         $match = str_replace("{", "\\{", $match);
         $match = str_replace("}", "}\\", $match);
         $keys = [];
-        foreach($this->getClients(self::CLIENTS_ALL) as $client) {
+        foreach ($this->getClients(self::CLIENTS_ALL) as $client) {
             //$keys[$client->getHumanId()] = [];
             foreach (new Iterator\Keyspace($client->getPredis(), $match, $count) as $key) {
                 //$keys[$client->getHumanId()][] = $key;
@@ -252,7 +265,7 @@ class Redis implements ClientInterface
 
     public function flushall()
     {
-        foreach($this->getClients(self::CLIENTS_WRITEONLY) as $client) {
+        foreach ($this->getClients(self::CLIENTS_WRITEONLY) as $client) {
             $client->flushall();
         }
     }
@@ -261,14 +274,14 @@ class Redis implements ClientInterface
     {
         #\Kint::dump($method, $arguments);
 
-        if(isset($arguments[0])) {
-            if(isset($arguments[0][0])) {
-                if(is_array($arguments[0])) {
+        if (isset($arguments[0])) {
+            if (isset($arguments[0][0])) {
+                if (is_array($arguments[0])) {
                     $affectedKeys = array_values($arguments[0]);
-                }else{
+                } else {
                     $affectedKeys = [$arguments[0]];
                 }
-            }else{
+            } else {
                 $affectedKeys = array_keys($arguments[0]);
                 $affectedValues = $arguments[0];
             }
@@ -276,9 +289,9 @@ class Redis implements ClientInterface
         }
 
         // If we have affected keys, lets work out what nodes they go to.
-        if(isset($affectedKeys)){
+        if (isset($affectedKeys)) {
             $mappedKeys = [];
-            foreach($affectedKeys as $key){
+            foreach ($affectedKeys as $key) {
                 $hash = $this->clusterStrategy->getSlotByKey($key);
                 $server = $this->getServerByHash($hash);
                 $mappedKeys[$key] = array_filter([
@@ -291,104 +304,104 @@ class Redis implements ClientInterface
             #\Kint::dump($mappedKeys);
         }
 
-        if(!isset($mappedKeys)){
+        if (!isset($mappedKeys)) {
             throw new Exception("Mapping keys failed!");
-        }else{
-            // Okay, so we mapped 'em. Lets create n-nodes iterations of this __call, one for each server affected
-            $mappedServers = [];
-            $mappedServerQueues = [];
-            $mappedServerConnections = [];
-            foreach ($mappedKeys as $key => $mappedKey) {
-                $mappedServers[$mappedKey['client']->getConnection()][$mappedKey['hash']][] = [
-                    'key' => $key,
-                    'value' => $mappedKey['value'] ?? null,
-                    'hash' => $mappedKey['hash']
-                ];
-                if(isset($mappedKey['value'])) {
-                    $mappedServerQueues[$mappedKey['client']->getConnection()][$mappedKey['hash']][$key] = $mappedKey['value'];
-                }else{
-                    $mappedServerQueues[$mappedKey['client']->getConnection()][$mappedKey['hash']][] = $key;
-                }
-                if(!isset($mappedServerConnections[$mappedKey['client']->getConnection()])){
-                    $mappedServerConnections[$mappedKey['client']->getConnection()]
-                        = $mappedKey['client'];
-                }
-            }
+        }
 
-            #\Kint::$max_depth = 4;
-            #\Kint::dump(
-            #    $mappedServers,
-            #    self::$clusterConfiguration,
-            #    array_keys($mappedServers),
-            #    $mappedServerQueues,
-            #    $mappedServerConnections
+        // Okay, so we mapped 'em. Lets create n-nodes iterations of this __call, one for each server affected
+        $mappedServers = [];
+        $mappedServerQueues = [];
+        $mappedServerConnections = [];
+        foreach ($mappedKeys as $key => $mappedKey) {
+            $mappedServers[$mappedKey['client']->getConnection()][$mappedKey['hash']][] = [
+                'key' => $key,
+                'value' => $mappedKey['value'] ?? null,
+                'hash' => $mappedKey['hash']
+            ];
+            if (isset($mappedKey['value'])) {
+                $mappedServerQueues[$mappedKey['client']->getConnection()][$mappedKey['hash']][$key] = $mappedKey['value'];
+            } else {
+                $mappedServerQueues[$mappedKey['client']->getConnection()][$mappedKey['hash']][] = $key;
+            }
+            if (!isset($mappedServerConnections[$mappedKey['client']->getConnection()])) {
+                $mappedServerConnections[$mappedKey['client']->getConnection()]
+                    = $mappedKey['client'];
+            }
+        }
+
+        $responses = [];
+        foreach ($mappedServerConnections as $serverName => $client) {
+            /** @var $client Client */
+
+            #echo sprintf(
+            #    "Connecting to %s to call %s for %d sub-elements\n",
+            #    $client->getHumanId(),
+            #    strtoupper($method),
+            #    count($mappedServerQueues[$serverName])
             #);
+            #\Kint::dump($mappedServerQueues[$serverName]);
 
-            $responses = [];
-            foreach($mappedServerConnections as $serverName => $client){
-                /** @var $client Client */
+            foreach ($mappedServerQueues[$serverName] as $hash => $items) {
+                // if its a single argument command, just send the arguments as-is,
+                // else send our processed items
+                $redirectedArguments = in_array($method, self::SINGLE_ARG_COMMANDS)
+                    ? $arguments
+                    : [0 => $items];
 
-                #echo sprintf(
-                #    "Connecting to %s to call %s for %d sub-elements\n",
-                #    $client->getHumanId(),
-                #    strtoupper($method),
-                #    count($mappedServerQueues[$serverName])
-                #);
-                #\Kint::dump($mappedServerQueues[$serverName]);
+                #\Kint::dump($method, $hash, $items, $redirectedArguments);
 
-                foreach($mappedServerQueues[$serverName] as $hash => $items) {
-                    // if its a single argument command, just send the arguments as-is,
-                    // else send our processed items
-                    $redirectedArguments = in_array($method, self::SINGLE_ARG_COMMANDS)
-                        ? $arguments
-                        : [0 => $items]
-                    ;
+                $response = $client->getPredis()
+                    ->__call(
+                        $method,
+                        $redirectedArguments
+                    );
 
-                    #\Kint::dump($method, $hash, $items, $redirectedArguments);
-
-                    $response = $client->getPredis()
-                        ->__call(
-                            $method,
-                            $redirectedArguments
-                        );
-
-                    // If its a single arg command, return fast with the response.
-                    if(in_array($method, self::SINGLE_ARG_COMMANDS)) {
-                        return $response;
-                    }
-
-                    // If its not, add it to our list of responses.
-                    $responses[] = $response;
-                }
-            }
-
-            #\Kint::dump($method, $responses);
-            $mergedResponses = [];
-            foreach($responses as $response){
-                if(is_array($response)) {
-                    $mergedResponses = array_merge($mergedResponses, $response);
-                }else{
-                    $mergedResponses[] = $response;
-                }
-                if($response instanceof Status && $response->getPayload() != 'OK'){
+                // If its a single arg command, return fast with the response.
+                if (in_array($method, self::SINGLE_ARG_COMMANDS)) {
                     return $response;
                 }
+
+                // If its not, add it to our list of responses.
+                $responses[] = $response;
             }
-            #\Kint::dump($mergedResponses);
-            if(reset($mergedResponses) instanceof Status){
-                return reset($mergedResponses);
-            }
-            sort($mergedResponses);
-            return $mergedResponses;
         }
+
+        #\Kint::dump($method, $responses);
+        $mergedResponses = [];
+        foreach ($responses as $response) {
+            if (is_array($response)) {
+                $mergedResponses = array_merge($mergedResponses, $response);
+            } else {
+                $mergedResponses[] = $response;
+            }
+            if ($response instanceof Status && $response->getPayload() != 'OK') {
+                return $response;
+            }
+        }
+        #\Kint::dump($mergedResponses);
+        if (reset($mergedResponses) instanceof Status) {
+            return reset($mergedResponses);
+        }
+        sort($mergedResponses);
+        return $mergedResponses;
+
     }
 
-    protected const SINGLE_ARG_COMMANDS=["get","set"];
-
-    public function __clearPerfLog() : void
+    public function __clearPerfLog(): void
     {
         $this->perfLog = [];
     }
+
+    public function __getPerfLogAsString(): string
+    {
+        $string = '';
+        foreach ($this->__getPerfLog() as $item) {
+            $string .= $item->__toString();
+        }
+
+        return $string;
+    }
+
     /**
      * @return PerfLogItem[]
      */
@@ -397,61 +410,12 @@ class Redis implements ClientInterface
         return $this->perfLog;
     }
 
-    public function __getPerfLogAsString() : string
-    {
-        $string = '';
-        foreach($this->__getPerfLog() as $item){
-            $string .= $item->__toString();
-        }
-
-        return $string;
-    }
-
-    protected function isMethodWriting($method): bool
-    {
-        // Okay, so given that we can -default- to writing, we're just gonna single out the things that are NOT
-        // a write method, and return false for them, otherwise return true.
-
-        #$methods = $this->getClient()->getPredis()->getProfile()->getSupportedCommands();
-        #ksort($methods);
-        #!\Kint::dump( $methods);
-
-        switch ($method) {
-            case 'GET':
-            case 'GETBIT':
-            case 'GETRANGE':
-            case 'HGET':
-            case 'HGETALL':
-            case 'HKEYS':
-            case 'HMGET':
-            case 'HSCAN':
-            case 'MGET':
-            case 'LLEN':
-            case 'LRANGE':
-            case 'SCAN':
-            case 'SSCAN':
-                return false;
-            default:
-                return true;
-        }
-    }
-
     protected function getClientByMoved($movedStatement): ?Client
     {
         $movedStatement = explode(" ", $movedStatement);
         $address = "tcp://{$movedStatement[2]}";
 
         return $this->getClientByAddress($address);
-    }
-
-    protected function getClientByAddress($address) : ?Client
-    {
-        foreach ($this->getClients(self::CLIENTS_ALL) as $client) {
-            if (in_array($address, $client->getConnectionDetails())) {
-                return $client;
-            }
-        }
-        return null;
     }
 
 
